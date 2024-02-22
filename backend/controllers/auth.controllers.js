872cc48056dec6f93fcb4c62d32296cbda485002
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-23 02:50:26 +0530

    Login API testing

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,10 +2,12 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 
+// Logout Feature
 export const logout = (req, res) => {
   console.log("Logout User");
 };
 
+// SignUp Feature
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
@@ -54,6 +56,8 @@ export const signup = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+// Login Feature
 export const login = async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -66,6 +70,15 @@ export const login = async (req, res) => {
     if (!user || !isPasswordCorrect) {
       return res.status(400).json({ error: "Invalid username or password" });
     }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
   } catch (error) {
     console.log("Error in Login controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
