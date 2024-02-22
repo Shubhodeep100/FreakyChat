576400fc020524169e1b85bcf0fd760ed8ca3f96
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-23 02:22:10 +0530

    Adding JWT Token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -3,5 +3,11 @@ const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "15d",
   });
-  res.cookie("jwt", token, {});
+  res.cookie("jwt", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    httpOnly: true, //prevent acces as attacks.
+    sameSite: "strict"
+  });
 };
+
+export default generateTokenAndSetCookie
