5d6ee93eb296c746172a784b3cd1d34e197450a7
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-23 00:54:17 +0530

    auth gender pirofilepic updates

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,4 +1,4 @@
-import User from '../models/user.model.js'
+import User from "../models/user.model.js";
 
 export const login = (req, res) => {
   console.log("Login User");
@@ -9,18 +9,29 @@ export const logout = (req, res) => {
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
-    if(password !== confirmPassword) {
-      return res.status(400).json({error: "Passwords do not match"})
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords do not match" });
     }
 
-    const user = await User.findOne({username});
+    const user = await User.findOne({ username });
 
-    if(user) {
-      return res.status(400).json({error: "Username already exists"})
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
     }
 
     // Hash Password here
-    
+    // https://avatar-placeholder.iran.liara.run/
+    const boyProfilePics = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePics = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender == "male" ? boyProfilePics : girlProfilePics,
+    });
+
+    await newUser.save();
   } catch (error) {}
 };
