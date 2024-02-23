bbcc731fcc4ba8641e02444cddaf0377814b19b1
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-23 15:25:30 +0530

    conversation controller work

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -27,6 +27,8 @@ export const sendMessage = async (req, res) => {
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
+
+    res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller", error.message);
     res.status(500).json({ error: "Internal Server error" });
