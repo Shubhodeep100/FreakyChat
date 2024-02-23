4dbe1361dce638f6b72d01d9d4fce531c8af2d72
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-23 15:15:30 +0530

    message route work

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,10 @@
 export const sendMessage = async (req, res) => {
-  console.log("message sent");
+  try {
+    const { message } = req.body;
+    const { id } = req.params;
+    const senderId = req.user._id;
+  } catch (error) {
+    console.log("Error in sendMessage controller", error.message);
+    res.status(500).json({ error: "Internal Server error" });
+  }
 };
