3dbb147074daec9c02f9a16ee473c2c548bef867
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-23 11:24:52 +0530

    Logout API testing

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -3,8 +3,14 @@ import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 // Logout Feature
-export const logout = (req, res) => {
-  console.log("Logout User");
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // SignUp Feature
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,16 +4,19 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
   username: {
     type: String,
     require: true,
     unique: true,
   },
+
   password: {
     type: String,
     require: true,
     minLength: 6,
   },
+  
   gender: {
     type: String,
     require: true,
