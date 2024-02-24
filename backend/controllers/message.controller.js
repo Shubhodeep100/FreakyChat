05155236c2f7e21b8490147c063a41ddac4373e6
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-24 13:32:44 +0530

    Retriving messages

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -54,7 +54,10 @@ export const getMessages = async (req, res) => {
       },
     }).populate("messages"); //Not Reference, but actual message itself.
 
-    res.status(200).json(conversation.messages);
+    if (!conversation) return res.status(200).json([]);
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
   } catch (error) {
     console.log("Error in getMessages controller", error.message);
     res.status(500).json({ error: "Internal Server error" });
