ce0d05f4f4621b269912016e5a8901643eb1ce8f
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-24 13:57:08 +0530

    user Routes work

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +14,7 @@ app.use(express.json()); //To parse the incoming requests with JSON payloads (fr
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/usets", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
