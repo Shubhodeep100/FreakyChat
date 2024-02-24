cc5619e928dc0f09a4bcd505f4d1ca55c211179a
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-24 14:20:45 +0530

    user Routes work

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,8 @@
-import express from 'express';
-import { getUsersForSidebar } from '../controllers/user.controllers.js';
+import express from "express";
+import { getUsersForSidebar } from "../controllers/user.controllers.js";
+import protectRoute from "../middleware/protectRoute.js";
 const router = express.Router();
 
-router.get("/", protectRoute,getUsersForSidebar)
+router.get("/", protectRoute, getUsersForSidebar);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -14,7 +16,7 @@ app.use(express.json()); //To parse the incoming requests with JSON payloads (fr
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.use("/api/usets", userRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
