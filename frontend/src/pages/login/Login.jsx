f8e188298544dbdcbda094d9c75a14ad24abf456
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-28 23:37:23 +0530

    login page updates

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -3,9 +3,9 @@ function Login() {
   return (
     <div className="flex flex-col justify-center gap-1 items-center">
       <form className="flex flex-col gap-6 px-16 py-14 formCont  backdrop-filter backdrop-blur-lg bg-opacity-50 rounded-2xl">
-        <div className="flex items-center gap-1 justify-center">
+        <div className="flex items-center gap-3 justify-center">
           <img src={logo} alt="frenzychat" className="h-10 w-10" />
-          <h1 className="font-signature">FreakyChatt</h1>
+          <h1 className="text-2xl text-white font-serif">FreakyChatt</h1>
         </div>
         <input
           type="text"
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,7 +1,56 @@
-const SignUp = () => {
+import logo from "../assets/chatlogo.png";
+function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="flex flex-col justify-center gap-1 items-center ">
+      <form className="flex flex-col gap-6 px-10 py-14 formCont backdrop-filter backdrop-blur-lg bg-opacity-50 rounded-2xl">
+        <div className="flex items-center gap-3 justify-center">
+          <img src={logo} alt="frenzychat" className="h-10 w-10" />
+          <h1 className="text-2xl text-white font-serif">FreakyChatt</h1>
+        </div>
+        <input
+          type="text"
+          placeholder="Full Name"
+          name="username"
+          className="inputClass"
+          // onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          className="inputClass"
+          // onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="Password"
+          className="inputClass"
+          // onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          name="Password"
+          className="inputClass"
+          // onChange={(e) => handleChange(e)}
+        />
+        <a
+          href="#"
+          className="text-sm hover:underline hover:text-blue-600 inline-block"
+        >
+          Already have an account?
+        </a>
+
+        <button type="submit" className="button">
+          SignUp
+        </button>
+        {/* <span>
+          Already have an account ? <Link to="/login">Login</Link>
+        </span> */}
+      </form>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
