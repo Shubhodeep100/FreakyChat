c1a3aa7e5a4511bd415684a23bbbfcd9cc3fd578
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-29 01:07:24 +0530

    sidebar updates

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,7 +1,18 @@
+import { IoIosSearch } from "react-icons/io";
+
 const SearchInput = () => {
   return (
-    <div>SearchInput</div>
-  )
-}
+    <form className="flex items-center gap-2">
+      <input
+        type="text1"
+        placeholder="Search"
+        className="input input-bordered rounded-full"
+      />
+      <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+        <IoIosSearch className="text-xl"/>
+      </button>
+    </form>
+  );
+};
 
-export default SearchInput
+export default SearchInput;
