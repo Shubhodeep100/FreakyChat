6f564a74ddcab1ff8a08df07550156b824e1208f
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-29 00:12:47 +0530

    sidebar updates

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,10 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+
 const Home = () => {
   return (
     <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter-lg bg-opacity-0 ">
-      
+      <Sidebar/>
+      {/* <MessageConatiner/> */}
     </div>
   );
 };
