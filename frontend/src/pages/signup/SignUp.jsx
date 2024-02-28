166cafd63f9317edb57098d10007ece270224d2e
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-28 23:45:10 +0530

    login page updates

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,8 +1,8 @@
 import logo from "../assets/chatlogo.png";
 function SignUp() {
   return (
-    <div className="flex flex-col justify-center gap-1 items-center ">
-      <form className="flex flex-col gap-6 px-10 py-14 formCont backdrop-filter backdrop-blur-lg bg-opacity-50 rounded-2xl">
+    <div className="flex flex-col justify-center gap-1 items-center min-w-96">
+      <form className="flex flex-col gap-6 px-10 py-14 formCont w-full backdrop-filter backdrop-blur-lg bg-opacity-50 rounded-2xl">
         <div className="flex items-center gap-3 justify-center">
           <img src={logo} alt="frenzychat" className="h-10 w-10" />
           <h1 className="text-2xl text-white font-serif">FreakyChatt</h1>
