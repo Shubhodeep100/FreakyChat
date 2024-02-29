636bcc7e9d9bba215ff09036a8875b3d91075c8e
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-29 23:38:01 +0530

    conversations updates

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,11 @@
+import { BiLogOut } from "react-icons/bi";
+
+const LogoutButton = () => {
+  return (
+    <div className="mt-auto">
+      <BiLogOut className="text-2xl hover:text-white cursor-pointer"/>
+    </div>
+  );
+};
+
+export default LogoutButton;
