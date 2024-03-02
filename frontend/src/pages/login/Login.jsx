85fe0d9690f08284eaa3dce0f9a8ea20f4a28de9
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-02 21:17:26 +0530

    UI updates

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -3,10 +3,10 @@ import logo from "../assets/chatlogo.png";
 function Login() {
   return (
     <div className="flex flex-col justify-center gap-1 items-center min-w-96">
-      <form className="flex flex-col gap-6 px-16 py-14 formCont w-full backdrop-filter backdrop-blur-lg bg-opacity-50 rounded-2xl">
+      <form className="flex flex-col gap-7 px-16 py-14 formCont w-full backdrop-filter backdrop-blur-lg bg-opacity-50 rounded-2xl">
         <div className="flex items-center flex-col gap-3 justify-center">
           <img src={logo} alt="frenzychat" className="h-10 w-10" />
-          <h1 className="text-2xl text-white tracking-wide font-signature">
+          <h1 className="text-3xl text-white tracking-wide font-signature">
             Freaky<span className="text-green-500">Chatt</span>
           </h1>
         </div>
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -22,12 +22,12 @@ const SignUp = () => {
   return (
     <div className="flex flex-col justify-center gap-1 items-center min-w-96">
       <form
-        className="flex flex-col gap-6 px-10 py-6 formCont w-full backdrop-filter backdrop-blur-lg bg-opacity-50 rounded-2xl"
+        className="flex flex-col gap-5 px-10 py-6 formCont w-full backdrop-filter backdrop-blur-lg bg-opacity-50 rounded-2xl"
         onSubmit={handleSubmit}
       >
         <div className="flex items-center gap-3 justify-center">
           <img src={logo} alt="frenzychat" className="h-10 w-10" />
-          <h1 className="text-2xl text-white tracking-wide font-signature">
+          <h1 className="text-3xl text-white tracking-wide font-signature">
             Freaky<span className="text-green-500">Chatt</span>
           </h1>
         </div>
@@ -70,7 +70,7 @@ const SignUp = () => {
           onCheckboxChange={handleCheckboxChange}
           selectedGender={inputs.gender}
         />
-        
+
         <div>
           <Link
             to="/login"
