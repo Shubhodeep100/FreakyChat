077a0bc4c962ccfa9d6e446c6b0622dcfd7402d0
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-02 14:23:43 +0530

    mesages list updates

--- frontend/src/components/sidebar/messages/MessageContainer.jsx
+++ frontend/src/components/sidebar/messages/MessageContainer.jsx
@@ -1,9 +1,18 @@
-const MessageContainer = () =>  {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import Messages from "./Messages";
 
-export default MessageContainer
+const MessageContainer = () => {
+  return (
+    <div className="md:min-w-[450px] flex flex-col">
+      <>
+        <div className="bg-slate-500 px-4 mb-2">
+          <span className="label-text">To: {" "}</span>
+          <span className="text-gray-900 font-semibold">Shubhodeep</span>
+         
+        </div>
+        <Messages/>
+      </>
+    </div>
+  );
+};
+
+export default MessageContainer;
