75640bdda169206b0f775da2af4e96a7a7a07b71
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-02 16:32:31 +0530

    routes setup

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -3,7 +3,7 @@ function Login() {
   return (
     <div className="flex flex-col justify-center gap-1 items-center min-w-96">
       <form className="flex flex-col gap-6 px-16 py-14 formCont w-full backdrop-filter backdrop-blur-lg bg-opacity-50 rounded-2xl">
-        <div className="flex items-center gap-3 justify-center">
+        <div className="flex items-center flex-col gap-3 justify-center">
           <img src={logo} alt="frenzychat" className="h-10 w-10" />
           <h1 className="text-2xl text-white tracking-wide font-signature">
             Freaky<span className="text-green-500">Chatt</span>
