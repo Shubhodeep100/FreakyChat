417c95384094a053ffa1e031bbb557ae18de8f93
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-02 14:57:34 +0530

    cleanup updates

--- frontend/src/components/sidebar/messages/MessageContainer.jsx
+++ frontend/src/components/sidebar/messages/MessageContainer.jsx
@@ -26,7 +26,7 @@ export default MessageContainer;
 
 const NoChatSelected = () => {
   return (
-    <div className="flex w-full items-center justify-center h-full">
+    <div className="flex items-center justify-center h-full w-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
         <p>Hello Shubhodeep!</p>
         <p>
@@ -37,7 +37,7 @@ const NoChatSelected = () => {
           </span>
         </p>
         <p>Select a Chat to start messaging</p>
-        <TiMessages className="text-3xl md:text-6xl text-center" />
+        <TiMessages className="text-3xl md:text-6xl text-center " />
       </div>
     </div>
   );
