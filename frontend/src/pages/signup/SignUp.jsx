7d61e9152855c7bf23d75bacb9e6acc3cd66004f
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-02 20:50:23 +0530

    signup data updates

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,10 +1,30 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/chatlogo.png";
 import GenderCheckBox from "./GenderCheckBox";
-function SignUp() {
+import { useState } from "react";
+const SignUp = () => {
+  const [inputs, setInputs] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+
+  const handleCheckboxChange = (gender) => {
+    setInputs({ ...inputs, gender });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
+
   return (
     <div className="flex flex-col justify-center gap-1 items-center min-w-96">
-      <form className="flex flex-col gap-6 px-10 py-6 formCont w-full backdrop-filter backdrop-blur-lg bg-opacity-50 rounded-2xl">
+      <form
+        className="flex flex-col gap-6 px-10 py-6 formCont w-full backdrop-filter backdrop-blur-lg bg-opacity-50 rounded-2xl"
+        onSubmit={handleSubmit}
+      >
         <div className="flex items-center gap-3 justify-center">
           <img src={logo} alt="frenzychat" className="h-10 w-10" />
           <h1 className="text-2xl text-white tracking-wide font-signature">
@@ -16,29 +36,41 @@ function SignUp() {
           placeholder="Full Name"
           name="username"
           className="inputClass"
-          // onChange={(e) => handleChange(e)}
+          value={inputs.fullName}
+          onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
         />
         <input
           type="text"
           placeholder="Username"
           name="username"
           className="inputClass"
-          // onChange={(e) => handleChange(e)}
+          value={inputs.username}
+          onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
         />
         <input
           type="password"
           placeholder="Password"
           name="Password"
           className="inputClass"
-          // onChange={(e) => handleChange(e)}
+          value={inputs.password}
+          onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
         />
         <input
           type="password"
           placeholder="Confirm Password"
           name="Password"
           className="inputClass"
-          // onChange={(e) => handleChange(e)}
+          value={inputs.confirmPassword}
+          onChange={(e) =>
+            setInputs({ ...inputs, confirmPassword: e.target.value })
+          }
         />
+
+        <GenderCheckBox
+          onCheckboxChange={handleCheckboxChange}
+          selectedGender={inputs.gender}
+        />
+        
         <div>
           <Link
             to="/login"
@@ -47,13 +79,13 @@ function SignUp() {
             Already have an account ?
           </Link>
         </div>
-        <GenderCheckBox />
+
         <button type="submit" className="button">
           SignUp
         </button>
       </form>
     </div>
   );
-}
+};
 
 export default SignUp;
