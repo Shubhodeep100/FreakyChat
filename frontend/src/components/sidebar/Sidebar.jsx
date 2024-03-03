d4cef773f7a89da420980a1fb032dd31a385ea16
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-03 16:38:14 +0530

    Logout functionality

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,15 +1,23 @@
+import useLogout from "../../hooks/useLogout";
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
+  const { loading, logout } = useLogout();
   return (
     <div className="border-r border-slate-500 p-4 flex flex-col">
       <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
-
-      <LogoutButton/>
+      {!loading ? (
+        <div className="flex flex-row gap-3 group" onClick={logout}>
+          <LogoutButton />
+          <p className="group-hover:cursor-pointer">Logout</p>
+        </div>
+      ) : (
+        <span className="loading loading-spinner"></span>
+      )}
     </div>
   );
 };
