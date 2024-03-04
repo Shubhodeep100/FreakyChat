1cf39271aa204138afcbe6fdab3172990117ad78
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-04 12:49:46 +0530

    dynamic conversations updates

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,24 +1,21 @@
-const Conversation = ({conversation, lastIdx}) => {
+const Conversation = ({ conversation, lastIdx }) => {
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-zinc-800 rounded p-2 py-1 cursor-pointer">
+      <div className="flex gap-2 items-center hover:bg-zinc-800 rounded p-2 py-2 cursor-pointer">
         <div className="avatar online">
           <div className="w-10 rounded-full">
-            <img
-              src={conversation.profilePic}
-              alt="Avatar"
-            />
+            <img src={conversation.profilePic} alt="Avatar" />
           </div>
         </div>
 
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
             <p className=" text-gray-200">{conversation.fullName}</p>
-          
           </div>
         </div>
       </div>
-      <div className="divider my-0 py-0 h-1" />
+
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
     </>
   );
 };
