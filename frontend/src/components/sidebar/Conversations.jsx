74bac79985fdb031db9f7be80807bb3dfe21fece
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-04 12:20:57 +0530

    Conversations hook

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,6 +1,8 @@
 import Conversation from "./Conversation"
 
 const Conversations = () => {
+
+  useGetConversations()
   return (
     <div className="py-2 flex flex-col overflow-auto bg-zinc-900">
       <Conversation />
