21b8f099c2fc49e472d3bbe27ca5baf9e34b49d3
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-04 12:28:47 +0530

    dynamic conversations updates

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,23 +1,19 @@
-import Conversation from "./Conversation"
+import useGetConversations from "../../hooks/useGetConversations";
+import Conversation from "./Conversation";
 
 const Conversations = () => {
-
-  useGetConversations()
+  const { loading, conversations } = useGetConversations();
+  console.log("Conversations:", conversations);
   return (
     <div className="py-2 flex flex-col overflow-auto bg-zinc-900">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conversation)=> (
+        <Conversation key={conversation._id} conversation={conversation} />
+      ))}
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
     </div>
   );
-}
+};
 
-export default Conversations
+export default Conversations;
