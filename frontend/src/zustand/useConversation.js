b19c4529c53980012c3ae64c183d8c99c0be5189
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-04 12:09:39 +0530

    zustand updates

--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -2,4 +2,8 @@ import {create} from 'zustand';
 const useConversation= create((set) =>({
     selectedConversation: null,
     setSelectedConversation: (selectedConversation) => set({selectedConversation}),
+    message: [],
+    setMessage: (message) => set({message}),
 }))
+
+export default useConversation;
