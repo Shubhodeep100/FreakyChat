3178f4cfeb10ce7c04f7a6f7cb2afa082bfc9b8e
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-04 14:49:39 +0530

    send message hook

--- frontend/src/components/sidebar/messages/MessageInput.jsx
+++ frontend/src/components/sidebar/messages/MessageInput.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { BsSend } from "react-icons/bs";
 
 const MessageInput = () => {
+  const [message, setMessage] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form className="px-4 my-3">
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
       <div className="w-full relative">
         <input
           type="text"
           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
           placeholder="Type a message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
         <button
           type="submit"
