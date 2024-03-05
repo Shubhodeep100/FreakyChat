19dbbe67f1b437f25e83d8b2d03711f6b9fba75f
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-05 12:51:07 +0530

    message chat updates

--- frontend/src/components/sidebar/messages/Message.jsx
+++ frontend/src/components/sidebar/messages/Message.jsx
@@ -1,28 +1,27 @@
-import {useAuthContext} from '../../../context/AuthContext';
-import useConversation from '../../../zustand/useConversation';
+import { useAuthContext } from "../../../context/AuthContext";
+import useConversation from "../../../zustand/useConversation";
 
 const Message = ({ message }) => {
   const { authUser } = useAuthContext();
 
-  const {selectedConversation} = useConversation();
+  const { selectedConversation } = useConversation();
   const fromMe = message.senderId === authUser._id;
-  const chatClasssName = fromMe ? 'chat-end' : 'chat-start';
-  const profilePic = fromMe ? authUser.profilePic : selectedConversation.profilePic;
+  const chatClasssName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-600" : "";
 
-  
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClasssName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img
-            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            alt="Tailwind char bubble component"
-          />
+          <img src={profilePic} alt="ProfilePic" />
         </div>
       </div>
       <div>
-        <div className={`chat-bubble text-white bg-blue-600`}>
-          Hii! whats up?
+        <div className={`chat-bubble text-white bg-blue-600 ${bubbleBgColor}`}>
+          {message.message}
         </div>
         <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
           12:45
