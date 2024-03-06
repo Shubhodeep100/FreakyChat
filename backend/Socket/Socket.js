20b2297c8d1e46126da7302ade1933db0cb9dc52
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-06 15:47:58 +0530

    Integrating Socket.io

--- backend/Socket/Socket.js
+++ backend/Socket/Socket.js
@@ -3,6 +3,18 @@ import http from "http";
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:3000"],
+        methods: ["GET","POST"]
+    },
+});
+
+io.on('connection',(socket) => {
+    console.log('a user connected', socket.id);
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+})
 
 export { app, io, server };
