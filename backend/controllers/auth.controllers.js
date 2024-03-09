22cb65dcf0e0e28630092349bff579f66b30338b
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-09 12:34:47 +0530

    bug fixes

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,15 +1,13 @@
-import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 
-
-// SignUp Feature
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
 
     if (password !== confirmPassword) {
-      return res.status(400).json({ error: "Passwords do not match" });
+      return res.status(400).json({ error: "Passwords don't match" });
     }
 
     const user = await User.findOne({ username });
@@ -18,11 +16,11 @@ export const signup = async (req, res) => {
       return res.status(400).json({ error: "Username already exists" });
     }
 
-    // Hash Password here
+    // HASH PASSWORD HERE
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // https://avatar-placeholder.iran.liara.run/
+
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
@@ -33,9 +31,9 @@ export const signup = async (req, res) => {
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
-    
+
     if (newUser) {
-      // Generate JWT Token
+      // Generate JWT token here
       generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
 
@@ -46,15 +44,14 @@ export const signup = async (req, res) => {
         profilePic: newUser.profilePic,
       });
     } else {
-      res.status(400).json({ error: "Invalid User Data" });
+      res.status(400).json({ error: "Invalid user data" });
     }
   } catch (error) {
+    console.log("Error in signup controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-
-// Login Feature
 export const login = async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -77,17 +74,17 @@ export const login = async (req, res) => {
       profilePic: user.profilePic,
     });
   } catch (error) {
+    console.log("Error in login controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-
-// Logout Feature
 export const logout = (req, res) => {
   try {
     res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
+    console.log("Error in logout controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { getUsersForSidebar } from "../controllers/user.controllers.js";
 import protectRoute from "../middleware/protectRoute.js";
+import { getUsersForSidebar } from "../controllers/user.controllers.js";
 const router = express.Router();
 
 router.get("/", protectRoute, getUsersForSidebar);
