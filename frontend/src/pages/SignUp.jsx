9d317797d4c5c174a6a182add14aafcb8291f481
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-09 11:06:02 +0530

    bug fixes

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/chatlogo.png";
-import GenderCheckBox from "./GenderCheckBox";
+import GenderCheckBox from "./signup/GenderCheckBox.jsx";
 import { useState } from "react";
 import { motion } from "framer-motion";
 import chaticon from "../../assets/chat.png";
-import useSignup from "../../hooks/useSignup.js"
+import useSignup from "../hooks/useSignup.js";
 const SignUp = () => {
   const transition = { type: "spring", duration: 3 };
   const [inputs, setInputs] = useState({
