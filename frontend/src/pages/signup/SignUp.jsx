b0eb98ffd84f5a3721ea550ea73350a707d3a707
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-10 00:27:10 +0530

    signup test

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 import GenderCheckBox from "./GenderCheckBox";
 import { useState } from "react";
-import useSignUp from "../../hooks/useSignUp";
+// import useSignUp from "../../hooks/useSignUp";
 import logo from "../../assets/chatlogo.png";
 import { motion } from "framer-motion";
 import chaticon from "../../assets/chat.png";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../../context/AuthContext";
+
+
 
 const SignUp = () => {
   const [inputs, setInputs] = useState({
@@ -134,3 +138,82 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
+
+// SignUp hook
+
+const useSignUp = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const signup = async ({
+    fullName,
+    username,
+    password,
+    confirmPassword,
+    gender,
+  }) => {
+    const success = handleInputErrors({
+      fullName,
+      username,
+      password,
+      confirmPassword,
+      gender,
+    });
+    if (!success) return;
+
+    setLoading(true);
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          fullName,
+          username,
+          password,
+          confirmPassword,
+          gender,
+        }),
+      });
+
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      setAuthUser(data);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, signup };
+};
+// export default useSignUp;
+
+function handleInputErrors({
+  fullName,
+  username,
+  password,
+  confirmPassword,
+  gender,
+}) {
+  if (!fullName || !username || !password || !confirmPassword || !gender) {
+    toast.error("Please fill in all fields");
+    return false;
+  }
+
+  if (password !== confirmPassword) {
+    toast.error("Passwords do not match");
+    return false;
+  }
+
+  if (password.length < 6) {
+    toast.error("Password must be at least 6 characters");
+    return false;
+  }
+
+  return true;
+}
