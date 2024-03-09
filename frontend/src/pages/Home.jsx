03dc493f99bd60b7c1215ff8cfc278e59c171e10
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-09 11:37:11 +0530

    Hooks imports

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import Sidebar from "../../components/sidebar/Sidebar";
-import MessageContainer from "../../components/sidebar/messages/MessageContainer";
+import Sidebar from "../components/sidebar/Sidebar";
+import MessageContainer from "../components/sidebar/messages/MessageContainer";
 
 const Home = () => {
   return (
